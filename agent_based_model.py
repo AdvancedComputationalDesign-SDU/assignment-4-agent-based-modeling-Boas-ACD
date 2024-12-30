b166627d12ab7129104ddca87fc36d0908849540
Boas-ACD <boas.olesen@gmail.com>
2024-12-30 10:43:37 +0100

    Updated the script
    updated it so instead of having each function in its own component. I created 1 component in grasshopper, which contain both the agent and simulation.

--- agent_based_model.py
+++ agent_based_model.py
@@ -36,7 +36,42 @@ slow_radius: Proximity for the agent to the target to slow down
 """
 
 # Agent import
-agent = Agent(srf, start_position)
+class Agent:
+    def __init__(self, srf, start_position):
+        self.position = rg.Point3d(*start_position) # to ensure correct datatype
+        self.velocity = rg.Vector3d(0, 0, 0) # to ensure correct datatype
+        self.srf = srf
+        self.avoidance_direction = None  # Keeps track of the current direction to go around obstacle
+
+    def dist(self, target_point):
+        """
+        Calculate distance between between agent & target
+        """
+        target = rg.Point3d(*target_point) # to ensure correct datatype
+        return self.position.DistanceTo(target)
+
+    def move(self):
+        "Move & update the agent's position"
+        # Update position
+        self.position += self.velocity
+
+    def seek_target(self, target, max_speed, slow_radius):
+        """
+        Adjust velocity to seek target
+        """
+        target_point = rg.Point3d(*target)
+        target_vector = target_point - self.position  # Vector pointing to the target
+        distance = target_vector.Length  # Distance to the target
+
+        if distance > 0:
+            target_vector.Unitize()  # Normalize
+            desired_velocity = target_vector * max_speed
+            if distance < slow_radius:
+                # Slow down as it approaches the target
+                desired_velocity *= distance / slow_radius
+            self.velocity = desired_velocity
+    
+    
 
 # Simulate agent seeking the goal
 prev_position = agent.position  # Store previous position for path tracing
